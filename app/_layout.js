858e495ccef5ddf3c103377de31072057a9c9513
Henriquenm <rm564699fiap@gmail.com>
2026-03-26 16:56:51 -0300

    criacao e alteracoes do _layout.js

---
+++ app/_layout.js
@@ -0,0 +1,19 @@
+import { Stack } from 'expo-router';
+import { Colors } from '../constants/Colors';
+
+export default function Layout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.primary },
+        headerTintColor: '#fff',
+        headerTitleStyle: { fontWeight: 'bold' },
+        headerTitle: 'FIAPressa',
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: 'Início' }} />
+      <Stack.Screen name="details" options={{ title: 'Cardápio Digital' }} />
+      <Stack.Screen name="profile" options={{ title: 'Meus Pedidos' }} />
+    </Stack>
+  );
+}a
